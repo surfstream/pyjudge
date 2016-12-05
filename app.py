b21606b9b29d8cc0c13e15abae67efb97da1085a
Mourya Kodidela <kodidelamourya@gmail.com>
2016-12-05 18:22:12 -0500

    pylint in progress

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ to run submitted code from the client.
 
 engine = create_engine('sqlite:///tutorial.db', echo=True)
 app = Flask(__name__)
-user="hello"
+user = "hello"
 polling_address = "http://localhost:5001/poll/pyjudge/demo/"
 tango_input = '../Tango/courselabs/pyjudge-demo/'
 tango_output = tango_input + 'output/'
@@ -43,7 +43,7 @@ def get_code(code, user):
     """
     Retrieve code from json sent by client send appropriate response
     """
-    if code == None:
+    if code is None:
         return 'Error: unable to retrieve data.'
     return create_temp_copy(user, code['code'])
 
@@ -62,11 +62,11 @@ def background_process():
             p = Process(target=submit, args=(user, fname))
             p.start()
             time.sleep(1)
-            pol = poll(polling_address+user+'.out/')
+            pol = poll(polling_address + user + '.out/')
             while 'Output file not found' in pol.text:
-                pol = poll(polling_address+user+'.out/')
+                pol = poll(polling_address + user + '.out/')
                 time.sleep(1)
-            clean_up(user, fname,tango_output)
+            clean_up(user, fname, tango_output)
             return jsonify(result=pol.text)
     except Exception as e:
         return jsonify(str(e))
@@ -84,12 +84,12 @@ def hint_process():
             return jsonify(result=fname)
         else:
             all_file = os.listdir(tango_input)
-            all_file = [tango_input+i for i in all_file if ".py" in i]
+            all_file = [tango_input + i for i in all_file if ".py" in i]
             res = find_similar(fname, all_file)
             output = ''
             for line in open(res, 'r'):
                 output += line
-            clean_up(user, fname,tango_output)
+            clean_up(user, fname, tango_output)
             return jsonify(result=output)
 
     except Exception as e:
@@ -98,27 +98,37 @@ def hint_process():
 
 @app.route('/login', methods=['POST'])
 def do_admin_login():
+    """
+    login and password validation and creating a Session
+    """
 
     POST_USERNAME = str(request.form['username'])
     POST_PASSWORD = str(request.form['password'])
 
     Session = sessionmaker(bind=engine)
     s = Session()
-    query = s.query(User).filter(User.username.in_([POST_USERNAME]), User.password.in_([POST_PASSWORD]) )
+    query = s.query(User).filter(
+        User.username.in_(
+            [POST_USERNAME]), User.password.in_(
+            [POST_PASSWORD]))
     result = query.first()
     if result:
         session['logged_in'] = True
         global user
-        user=POST_USERNAME
+        user = POST_USERNAME
     else:
         flash('wrong password!')
     return home()
 
+
 @app.route("/logout")
 def logout():
+    """
+    logging out from the Session back to the authentication page
+    """
     session['logged_in'] = False
     return home()
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
-    app.run(debug=True,host='0.0.0.0', port=4003)
+    app.run(debug=True, host='0.0.0.0', port=4003)
