072b56192970564347b7142bc4356ab08ffd5513
Luis Vega <luis.m.vega.a@gmail.com>
2016-12-04 20:43:27 -0500

    modularized some code shared between hint and submit procs and implemented hinting process, cleaned up main html, and edited hint/ReadMe for readability

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 import os
 import time
 from flask import Flask, flash, redirect, render_template, request, session, abort, jsonify
@@ -7,76 +8,100 @@ from tabledef import *
 from werkzeug import secure_filename
 from subprocess import run
 from multiprocessing import Process
-from submission import submit
+from submission import submit, create_temp_copy, clean_up
+import sys
+sys.path.insert(0, 'hint/')
+from find_similar import find_similar
+
+
+"""
+Main Flask Application, handles the rendering and AJAX calls between server and
+client as well as make the necessary calls to interface with the Tango server
+to run submitted code from the client.
+"""
 
 engine = create_engine('sqlite:///tutorial.db', echo=True)
 app = Flask(__name__)
 user="hello"
 polling_address = "http://localhost:5001/poll/pyjudge/demo/"
- 
+tango_input = '../Tango/courselabs/pyjudge-demo/'
+tango_output = tango_input + 'output/'
+
+
 @app.route('/')
 def home():
+    """
+    login portal
+    """
     if not session.get('logged_in'):
         return render_template('login.html')
     else:
         return render_template("index.html")
 
 
-@app.route("/background_process", methods=['GET', 'POST'])
+def get_code(code, user):
+    """
+    Retrieve code from json sent by client send appropriate response
+    """
+    if code == None:
+        return 'Error: unable to retrieve data.'
+    return create_temp_copy(user, code['code'])
+
+
+@app.route("/background_process", methods=['POST'])
 def background_process():
+    """
+    AJAX handling for user submission and interfacing with TANGO,
+    """
     try:
-        if request.method == 'GET':
-            lang = request.args.get('proglang', 0, type=str)
-            if lang.lower() == 'python':
-                return jsonify(result='You are wise')
-            else:
-                return jsonify(result='Try again.')
-        elif request.method == 'POST':
-            code = request.get_json()
-            if code == None:
-                return jsonify(result='Error detrieving data.')
-            else:
-                fname = user + '_primes.py'
-                f = open(fname, 'w')
-                f.write(code['code'])
-                f.close()
-                print("submission")
-                p = Process(target=submit, args=(user, fname))
-                p.start()
-                p.join()
+        code = request.get_json()
+        fname = get_code(code, user)
+        if 'Error:' in fname:
+            return jsonify(result=fname)
+        else:
+            p = Process(target=submit, args=(user, fname))
+            p.start()
+            time.sleep(1)
+            pol = poll(polling_address+user+'.out/')
+            while 'Output file not found' in pol.text:
                 pol = poll(polling_address+user+'.out/')
-                while 'Output file not found' in pol.text:
-                    pol = poll(polling_address+user+'.out/')
-                    time.sleep(1)
-                outf = open(user+'.out', 'w')
-                string = str(pol.text)
-                outf.write(string)
-                outf.close()
-                #outf = open("admin.out", 'w')
-                #for line in open('../Tango/courselabs/pyjudge-demo/output/'+user+'.out', 'r'):
-                #    outf.write(line)
-                #outf.close()
-                run(['rm', '../Tango/courselabs/pyjudge-demo/output/'+user+'.out'])
-                #return "hello World"
-                return jsonify(result=string)
+                time.sleep(1)
+            clean_up(user, fname,tango_output)
+            return jsonify(result=pol.text)
     except Exception as e:
-        return str(e)        
+        return jsonify(str(e))
 
 
-@app.route("/hint_process", methods=['GET', 'POST'])
+@app.route("/hint_process", methods=['POST'])
 def hint_process():
+    """
+    AJAX handling for user requesting hint
+    """
     try:
-        pass
+        code = request.get_json()
+        fname = get_code(code, user)
+        if 'Error:' in fname:
+            return jsonify(result=fname)
+        else:
+            all_file = os.listdir(tango_input)
+            all_file = [tango_input+i for i in all_file if ".py" in i]
+            res = find_similar(fname, all_file)
+            output = ''
+            for line in open(res, 'r'):
+                output += line
+            clean_up(user, fname,tango_output)
+            return jsonify(result=output)
+
     except Exception as e:
-        return str(e)
+        return jsonify(str(e))
 
 
 @app.route('/login', methods=['POST'])
 def do_admin_login():
- 
+
     POST_USERNAME = str(request.form['username'])
     POST_PASSWORD = str(request.form['password'])
- 
+
     Session = sessionmaker(bind=engine)
     s = Session()
     query = s.query(User).filter(User.username.in_([POST_USERNAME]), User.password.in_([POST_PASSWORD]) )
@@ -92,8 +117,8 @@ def do_admin_login():
 @app.route("/logout")
 def logout():
     session['logged_in'] = False
-    return home()    
- 
+    return home()
+
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
     app.run(debug=True,host='0.0.0.0', port=4003)
--- submission.py
+++ submission.py
@@ -1,8 +1,21 @@
 import os
+import time
 from subprocess import run
 
 def submit(user, user_file):
     user_job = '{"localFile" : "userfile", "destFile" : "primes.py"}'.replace("userfile", user_file)
     run(['bash', 'submit.sh', user_file, user_job, user])
-    
 
+def create_temp_copy(user, code):
+    fname = user + "_primes.py"
+    f = open(fname, 'w')
+    f.write(code)
+    f.close()
+    return fname
+
+def clean_up(user, fname, tango_output):
+    time.sleep(1)
+    run(['rm', fname])
+    time.sleep(1)
+    path = tango_output+user+'.out'
+    run(['rm', path])
