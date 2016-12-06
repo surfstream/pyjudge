fb77d917507357d75d86e958903595a1f8a584eb
Luis Vega <luis.m.vega.a@gmail.com>
2016-12-05 19:44:09 -0500

    adding in file documentation to my work in and additions

--- submission.py
+++ submission.py
@@ -1,21 +1,44 @@
-import os
+"""
+Helper moduel to handle temporary files as well as provide a clean call to
+interface with Tango server for submissions.
+"""
+
+
 import time
 from subprocess import run
 
+
 def submit(user, user_file):
-    user_job = '{"localFile" : "userfile", "destFile" : "primes.py"}'.replace("userfile", user_file)
+    """
+    Funtion to alter the a string in order to make the appropriate funtion call
+    in order to submit a job to the tango server.
+    """
+    user_job = '{"localFile" : "userfile", "destFile" : "primes.py"}'.replace(
+        "userfile", user_file)
     run(['bash', 'submit.sh', user_file, user_job, user])
 
+
 def create_temp_copy(user, code):
+    """
+    Function to create the temporary file from user submited code. In order to
+    create jobs for the tango server and/or find similar code to provide a hint
+    for the client.
+    """
     fname = user + "_primes.py"
-    f = open(fname, 'w')
-    f.write(code)
-    f.close()
+    user_file = open(fname, 'w')
+    user_file.write(code)
+    user_file.close()
     return fname
 
+
 def clean_up(user, fname, tango_output):
+    """
+    Function containing clean up code, to remove output file once displayed to
+    client to reduce storage, as well as allow Tango server some leeway to
+    properly terminate before clean up.
+    """
     time.sleep(1)
     run(['rm', fname])
     time.sleep(1)
-    path = tango_output+user+'.out'
+    path = tango_output + user + '.out'
     run(['rm', path])
--- submit.sh
+++ submit.sh
@@ -1,19 +1,36 @@
 #! /bin/bash
+: '
+This file runs the necessary commands to perform a proper submission on the code
+utilizing the provided default code of Tango, due to conflicts calling through
+python directly and timeconstraints to fully flesh out with the API to
+communicate with pyhton.
+'
 
+#store path, used to reference location of temporary code file
 path=$(pwd)
+
+#setup to interface with Tango
 cd ../Tango
 source bin/activate
+
+#check if user has previously submitted a solution for the problem
 filepath="courselabs/pyjudge-demo/${1}"
 if [ -f $filepath ]
 	then
+	    #if so delete, since Tango doesn't allow to directly overwrite
 		rm $filepath
 fi
+
+#"upload" the file to the Tango Server
 python clients/tango-rest.py -P 5001 -k "pyjudge" -l "demo" --upload --filename \
 "${path}/${1}"
 
+#Create and Run Job:
+#run the job on the docker image provided for Tango
 python clients/tango-rest.py -P 5001 -k "pyjudge" -l "demo" \
 --addJob --infiles \
 "$2" '{"localFile" : "autograde-Makefile", "destFile" : "Makefile"}' \
 --image "autograding_image" --outputFile "$3.out" --jobname "$3"
+#exit script
 deactivate
 
